52277bf6348ed8715eddf06ad2569d9495b5d2c8
Roopa Sreedhar A <193498568+coderRoopa@users.noreply.github.com>
2026-04-17 08:21:38 +0530

    Refactor monitor_ioctl.h to simplify IOCTL definitions

--- boilerplate/monitor_ioctl.h
+++ boilerplate/monitor_ioctl.h
@@ -1,25 +1,11 @@
 #ifndef MONITOR_IOCTL_H
 #define MONITOR_IOCTL_H
 
-#ifdef __KERNEL__
 #include <linux/ioctl.h>
-#include <linux/types.h>
-#else
-#include <sys/ioctl.h>
-#include <sys/types.h>
-#endif
 
-#define MONITOR_NAME_LEN 32
+#define IOCTL_BASE 'M'
 
-struct monitor_request {
-    pid_t pid;
-    unsigned long soft_limit_bytes;
-    unsigned long hard_limit_bytes;
-    char container_id[MONITOR_NAME_LEN];
-};
-
-#define MONITOR_MAGIC 'M'
-#define MONITOR_REGISTER _IOW(MONITOR_MAGIC, 1, struct monitor_request)
-#define MONITOR_UNREGISTER _IOW(MONITOR_MAGIC, 2, struct monitor_request)
+#define REGISTER_PID _IOW(IOCTL_BASE, 1, int)
+#define UNREGISTER_PID _IOW(IOCTL_BASE, 2, int)
 
 #endif
