d571ba0655d6f940aaae578add167d63976dd07a
Roopa Sreedhar A <193498568+coderRoopa@users.noreply.github.com>
2026-04-17 08:52:05 +0530

    ioctl commands and monitor_request struct
    Updated ioctl commands and added shared structure for monitor requests.

--- boilerplate/monitor_ioctl.h
+++ boilerplate/monitor_ioctl.h
@@ -2,10 +2,20 @@
 #define MONITOR_IOCTL_H
 
 #include <linux/ioctl.h>
+#include <linux/types.h>
 
-#define IOCTL_BASE 'M'
+#define DEVICE_NAME "container_monitor"
 
-#define REGISTER_PID _IOW(IOCTL_BASE, 1, int)
-#define UNREGISTER_PID _IOW(IOCTL_BASE, 2, int)
+/* ioctl commands */
+#define MONITOR_REGISTER   _IOW('a', 1, struct monitor_request)
+#define MONITOR_UNREGISTER _IOW('a', 2, struct monitor_request)
+
+/* shared structure */
+struct monitor_request {
+    pid_t pid;
+    unsigned long soft_limit_bytes;
+    unsigned long hard_limit_bytes;
+    char container_id[32];
+};
 
 #endif
